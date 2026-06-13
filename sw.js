a891f25b44e723bbfdd31701651445a7117bcab1
MSFkhanff <msfreefire846@gmail.com>
2026-06-13 03:34:50 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11141717
+    "zoneId": 11141810
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
